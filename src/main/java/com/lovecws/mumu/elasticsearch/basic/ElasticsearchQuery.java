cce9e4bf1fe1a6d187a349df1f6c2fe8be48694c
babymm <babymm@aliyun.com>
2018-07-11 18:26:21 +0800

    添加查询测试代码

--- src/main/java/com/lovecws/mumu/elasticsearch/basic/ElasticsearchQuery.java
+++ src/main/java/com/lovecws/mumu/elasticsearch/basic/ElasticsearchQuery.java
@@ -5,14 +5,16 @@ import com.lovecws.mumu.elasticsearch.client.ElasticsearchPool;
 import org.apache.log4j.Logger;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchScrollRequest;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -37,7 +39,7 @@ public class ElasticsearchQuery {
      * @param id        主键
      * @return
      */
-    public Map<String, Object> get(String indexName, String typeName, String id) {
+    public Map<String, Object> queryById(String indexName, String typeName, String id) {
         if (indexName == null || "".equals(indexName)) {
             return null;
         }
@@ -48,44 +50,17 @@ public class ElasticsearchQuery {
         TransportClient transportClient = elasticsearchClient.client();
         try {
             GetResponse getResponse = transportClient.get(new GetRequest(indexName, typeName, id)).get();
-            return getResponse.getSource();
+            if (getResponse.isExists()) {
+                return getResponse.getSource();
+            }
+            log.info("文档不存在{indexName:" + indexName + ",typeName:" + typeName + ",id:" + id + "}");
+            return new HashMap<>();
         } catch (InterruptedException | ExecutionException e) {
             log.error(e);
         } finally {
             pool.removeClient(elasticsearchClient);
         }
         return null;
-    }
-
-    /**
-     * term字段查询
-     *
-     * @param indexName
-     * @param typeName
-     * @param fieldName
-     * @param fieldValue
-     * @return
-     */
-    public List<Map<String, Object>> termQuery(String indexName, String typeName, String fieldName, Object fieldValue) {
-        ElasticsearchClient elasticsearchClient = pool.buildClient();
-        try {
-            TransportClient transportClient = elasticsearchClient.client();
-            TermQueryBuilder termQueryBuilder = new TermQueryBuilder(fieldName, fieldValue);
-            SearchResponse searchResponse = transportClient.prepareSearch(indexName)
-                    .setSearchType(SearchType.DEFAULT)
-                    .setTypes(typeName)
-                    .setQuery(termQueryBuilder)
-                    .setFrom(0)
-                    .setSize(10)
-                    .get();
-            List<Map<String, Object>> datas = new ArrayList<Map<String, Object>>();
-            for (SearchHit searchHit : searchResponse.getHits()) {
-                datas.add(searchHit.getSource());
-            }
-            return datas;
-        } finally {
-            pool.removeClient(elasticsearchClient);
-        }
     }
 
     /**
@@ -101,38 +76,130 @@ public class ElasticsearchQuery {
     public List<Map<String, Object>> scroll(String indexName, String typeName, String fieldName, Object fieldValue, int batchSize) {
         ElasticsearchClient elasticsearchClient = pool.buildClient();
         String scrollId = null;
-        long totalHits = 0;
+        long totalHits = batchSize;
         List<Map<String, Object>> datas = new ArrayList<Map<String, Object>>();
         try {
             TransportClient transportClient = elasticsearchClient.client();
             //查询获取到scrollId
-            SearchResponse searchResponse = transportClient.prepareSearch(indexName)
+            SearchRequestBuilder searchRequestBuilder = transportClient.prepareSearch(indexName)
                     .setTypes(typeName)
                     .setScroll("1m")
                     .setSearchType(SearchType.DEFAULT)
-                    .setQuery(new TermQueryBuilder(fieldName, fieldValue))
-                    .setSize(batchSize)
-                    .get();
+                    .setSize(batchSize);
+            if (fieldName != null && fieldValue != null) {
+                searchRequestBuilder.setQuery(new TermQueryBuilder(fieldName, fieldValue));
+            }
+            SearchResponse searchResponse = searchRequestBuilder.get();
             scrollId = searchResponse.getScrollId();
+            log.info("scrollId:" + scrollId);
             //scroll查询 当获取的数据量小于批处理数量 则退出scroll查询
-            while (totalHits < batchSize) {
+            while (totalHits == batchSize) {
                 SearchScrollRequest searchScrollRequest = new SearchScrollRequest(scrollId);
+                searchScrollRequest.scrollId(scrollId);
                 SearchResponse response = transportClient.searchScroll(searchScrollRequest).get();
-                totalHits = response.getHits().totalHits();
+                totalHits = response.getHits().totalHits;
                 scrollId = response.getScrollId();
                 for (SearchHit searchHit : searchResponse.getHits()) {
                     datas.add(searchHit.getSource());
                 }
             }
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | ExecutionException e) {
             log.error(e);
-        } catch (ExecutionException e) {
-            log.error(e);
+            e.printStackTrace();
         } finally {
             //清除scrollId
-            elasticsearchClient.client().prepareClearScroll().addScrollId(scrollId).get();
+            if (scrollId != null) {
+                elasticsearchClient.client().prepareClearScroll().addScrollId(scrollId).get();
+            }
             pool.removeClient(elasticsearchClient);
         }
         return datas;
     }
+
+    /**
+     * 基本查询
+     *
+     * @param indexName
+     * @param typeName
+     * @param queryBuilder
+     * @return
+     */
+    public List<Map<String, Object>> query(String indexName, String typeName, QueryBuilder queryBuilder) {
+        ElasticsearchClient elasticsearchClient = pool.buildClient();
+        try {
+            TransportClient transportClient = elasticsearchClient.client();
+            SearchResponse searchResponse = transportClient.prepareSearch(indexName)
+                    .setSearchType(SearchType.DEFAULT)
+                    .setTypes(typeName)
+                    .setQuery(queryBuilder)
+                    .setFrom(0)
+                    .setSize(10)
+                    .get();
+            List<Map<String, Object>> datas = new ArrayList<Map<String, Object>>();
+            for (SearchHit searchHit : searchResponse.getHits()) {
+                datas.add(searchHit.getSource());
+            }
+            return datas;
+        } finally {
+            pool.removeClient(elasticsearchClient);
+        }
+    }
+
+    /**
+     * term字段查询
+     *
+     * @param indexName
+     * @param typeName
+     * @param fieldName
+     * @param fieldValue
+     * @return
+     */
+    public List<Map<String, Object>> termQuery(String indexName, String typeName, String fieldName, Object fieldValue) {
+        TermQueryBuilder termQueryBuilder = new TermQueryBuilder(fieldName, fieldValue);
+        return query(indexName, typeName, termQueryBuilder);
+    }
+
+
+    /**
+     * term 多值查询
+     *
+     * @param indexName
+     * @param typeName
+     * @param fieldName
+     * @param fieldValue
+     * @return
+     */
+    public List<Map<String, Object>> termsQuery(String indexName, String typeName, String fieldName, Object... fieldValue) {
+        TermsQueryBuilder termsQueryBuilder = new TermsQueryBuilder(fieldName, fieldValue);
+        return query(indexName, typeName, termsQueryBuilder);
+    }
+
+    /**
+     * 多id查询
+     *
+     * @param indexName
+     * @param typeName
+     * @param ids
+     * @return
+     */
+    public List<Map<String, Object>> idsQuery(String indexName, String typeName, String... ids) {
+        IdsQueryBuilder idsQueryBuilder = new IdsQueryBuilder();
+        idsQueryBuilder.addIds(ids);
+        return query(indexName, typeName, idsQueryBuilder);
+    }
+
+    /**
+     * 匹配查询
+     *
+     * @param indexName
+     * @param typeName
+     * @param fieldName
+     * @param fieldValue
+     * @return
+     */
+    public List<Map<String, Object>> matchQuery(String indexName, String typeName, String fieldName, Object fieldValue) {
+        MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder(fieldName, fieldValue);
+        return query(indexName, typeName, matchQueryBuilder);
+    }
+
 }
---
+++ src/test/java/com/lovecws/mumu/elasticsearch/basic/ElasticsearchQueryTest.java
@@ -0,0 +1,50 @@
+package com.lovecws.mumu.elasticsearch.basic;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 甘亮
+ * @Description: 查询测试
+ * @date 2018/7/11 17:44
+ */
+public class ElasticsearchQueryTest {
+
+    private static final Logger log = Logger.getLogger(ElasticsearchQueryTest.class);
+
+    ElasticsearchQuery elasticsearchQuery = new ElasticsearchQuery();
+
+    @Test
+    public void queryById() {
+        Map<String, Object> stringObjectMap = elasticsearchQuery.queryById("gynetres",
+                "gynet_type",
+                "a8dd67022f5a47068394a8b8ea3d9864");
+        log.info(stringObjectMap);
+    }
+
+    @Test
+    public void termQuery() {
+        List<Map<String, Object>> mapList = elasticsearchQuery.termQuery("gynetres",
+                "gynet_type",
+                "id",
+                "1");
+        for (Map map : mapList) {
+            log.info(map);
+        }
+    }
+
+    @Test
+    public void scroll() {
+        List<Map<String, Object>> mapList = elasticsearchQuery.scroll("gynetres",
+                "gynet_type",
+                null,
+                null,
+                100);
+        for (Map map : mapList) {
+            log.info(map);
+        }
+    }
+}
